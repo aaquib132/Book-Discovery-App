db86662001d2671381e5620b2cfe87bfe86caea1
Aaquib Ahmad <khan850717@gmail.com>
2026-01-17 11:33:55 +0530

    Update API base URL to use environment variable

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://book-discovery-app.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export const searchBooks = (query) =>
