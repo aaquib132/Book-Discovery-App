080fbba04b6d0c94367323bb4ff9372122a0c372
Aaquib Ahmad <khan850717@gmail.com>
2026-01-17 11:02:04 +0530

    Update API base URL to include '/api' path

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://book-discovery-app.onrender.com"
+  baseURL: "https://book-discovery-app.onrender.com/api"
 });
 
 export const searchBooks = (query) =>
